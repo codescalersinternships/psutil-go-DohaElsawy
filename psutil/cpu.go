b0a0124450c163f863d25b66ee1a58fca70ab55b
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-26 16:03:13 +0300

    refactor: remove internal,abstract functions but not DI yet

--- psutil/cpu.go
+++ psutil/cpu.go
@@ -2,23 +2,120 @@ package psutil
 
 import (
 	"errors"
-
-	"github.com/codescalersinternships/psutil-go-DohaElsawy/internal"
+	"os"
+	"strconv"
+	"strings"
 )
 
 var (
-	ErrNotFindEnoughFields = errors.New("cpuinfo: did't find all data")
+	ErrFindEnoughFields = errors.New("cpuinfo: did't find all data")
 )
 
-func GetCpuInfo() (internal.CpuInfo,error) {
+type CpuInfo struct {
+	Vendor    string
+	MdoelName string
+	CacheSize string
+	CPUMHZ    float64
+	whenQuit  int
+}
 
-	var c internal.CpuInfo
-	err := c.ParseCpuData("/proc/cpuinfo")
+type cpuFile struct {
+	fileName string
+}
+
+
+type icpu interface {
+	loadCpudata() (string, error)
+}
+
+var _ icpu = (*cpuFile)(nil)
+
+func GetCpuInfo() (CpuInfo, error) {
+	file := newCpuFile("")
+
+	data, err := file.loadCpudata()
 
 	if err != nil {
-		return internal.CpuInfo{},err
+		return CpuInfo{},nil
 	}
 
-	return c ,nil
+	cpu := newCpu()
+
+	err = cpu.parseCpuData(data)
+
+	if err != nil {
+		return CpuInfo{} , err
+	}
+	return cpu, nil
+}
+
+func newCpuFile(path string) cpuFile {
+	return cpuFile{
+		fileName: path,
+	}
+}
+
+func newCpu() CpuInfo {
+	return CpuInfo{
+		Vendor:    "",
+		MdoelName: "",
+		CPUMHZ:    0,
+		CacheSize: "",
+		whenQuit:  4,
+	}
+}
+
+func (fcpu *cpuFile) loadCpudata() (string, error) {
+
+	data, err := os.ReadFile(fcpu.fileName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func (c *CpuInfo) parseCpuData(data string) error {
+
+	lines := strings.Split(data, "\n")
+
+	for _, line := range lines {
+
+		keyVal := strings.SplitN(line, ":",2)
+		key := keyVal[0]
+		value := keyVal[1]
+
+		key = strings.TrimSpace(key)
+		value = strings.TrimSpace(value)
+
+		if key == "vendor_id" {
+			c.Vendor = value
+			c.whenQuit--
+		}
+		if key == "cache size" {
+			c.CacheSize = value
+			c.whenQuit--
+		}
+		if key == "model name" {
+			c.MdoelName = value
+			c.whenQuit--
+		}
+		if key == "cpu MHz" {
+			convVal, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				return err
+			}
+			c.CPUMHZ = convVal
+			c.whenQuit--
+		}
+
+		if c.whenQuit == 0 {
+			return nil
+		}
+
+	}
+
+	return ErrFindEnoughFields
 
 }
--- psutil/cpu_test.go
+++ psutil/cpu_test.go
@@ -11,9 +11,28 @@ func TestCpuInfo(t *testing.T) {
 	t.Run("get actual data", func(t *testing.T) {
 
 		c , err := GetCpuInfo()
-		
+
 		assert.NoError(t, err)
 		assert.NotEmpty(t, c)
 	})
+
+}
+
+
+func TestCpu(t *testing.T) {
+
+	t.Run("valid case", func(t *testing.T) {
+
+	})
+}
+
+func TestLoaddata(t *testing.T) {
+	file := newCpuFile("./testdata/fakecpufile.txt")
+
+	data, err := file.loadCpudata()
+
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, data)
 
 }
