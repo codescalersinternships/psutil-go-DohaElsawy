276c00688cd7231b9bba21d0cbe9257f8b30b96d
dohaelsawy <dohaelsawy18@gmail.com>
2024-09-26 14:02:03 +0300

    fix: returning type of cpu info

--- psutil/cpu.go
+++ psutil/cpu.go
@@ -10,15 +10,15 @@ var (
 	ErrNotFindEnoughFields = errors.New("cpuinfo: did't find all data")
 )
 
-func GetCpuInfo() error {
+func GetCpuInfo() (internal.CpuInfo,error) {
 
 	var c internal.CpuInfo
 	err := c.ParseCpuData("/proc/cpuinfo")
 
 	if err != nil {
-		return err
+		return internal.CpuInfo{},err
 	}
 
-	return nil
+	return c ,nil
 
 }
--- psutil/cpu_test.go
+++ psutil/cpu_test.go
@@ -10,9 +10,10 @@ func TestCpuInfo(t *testing.T) {
 
 	t.Run("get actual data", func(t *testing.T) {
 
-		err := GetCpuInfo()
-
+		c , err := GetCpuInfo()
+		
 		assert.NoError(t, err)
+		assert.NotEmpty(t, c)
 	})
 
 }
